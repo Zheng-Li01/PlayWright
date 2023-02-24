347188b5450d06e0d4b43d38986df480c00f2a67
zheng Li <m18629016454@163.com>
2023-02-24 00:36:24 -0800

    write the first case

--- testcases/test_homepage.py
+++ testcases/test_homepage.py
@@ -19,3 +19,15 @@ def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_in
 
     # Expects the URL to contain intro.
     expect(page).to_have_url(re.compile(".*intro"))
+
+
+def test_playright_plugin(page: Page):
+    page.goto("https://playwright.dev/python/")
+    get_started = page.get_by_role("link", name="Get started")
+    get_started.click()
+
+    playwright_plugin= page.get_by_role("link",name="Playwright Pytest plugin")
+    # playwright_plugin.screenshot(animations="disabled", path="link.png")
+    expect(playwright_plugin).to_have_attribute("href", "/python/docs/test-runners")
+    playwright_plugin.click()
+    expect(page).to_have_url(re.compile(".*test-runners"))
