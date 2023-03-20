ee37a182b4d182cd430858364f94fccf8dfec7e7
zheng Li <m18629016454@163.com>
2023-03-20 03:05:25 -0700

    improve the prebuild case

--- testcases/test_prebuild.py
+++ testcases/test_prebuild.py
@@ -14,10 +14,11 @@ def test_prebuild_with_vscode_repo(playwright : Playwright):
     page.locator("summary").filter(has_text="Code").click()
     page.get_by_role("tab", name="Codespaces").click()
     with page.expect_popup() as new_page_info:
-        page.get_by_role("button", name="Create codespace on main").click()
+        page.get_by_role("form").filter(has_text="Create a codespace on main").get_by_role("button").click()
         page.wait_for_timeout(30000)
     new_page = new_page_info.value
-    new_page.wait_for_timeout(30000)
+    new_page.wait_for_selector(".notification-list-item-message", state="detached")
+    # new_page.wait_for_timeout(30000)
     terminaltextarea =  "#terminal > div > div > div.monaco-scrollable-element > div.split-view-container > div > div > div.pane-body.shell-integration.integrated-terminal.wide > div.monaco-split-view2.horizontal > div.monaco-scrollable-element > div.split-view-container > div > div > div > div > div > div.monaco-scrollable-element > div.split-view-container > div > div > div > div > div > div.xterm-screen > div.xterm-helpers > textarea"
     new_page.type(terminaltextarea,"clear")
     new_page.keyboard.press("Enter")
@@ -33,3 +34,4 @@ def test_prebuild_with_vscode_repo(playwright : Playwright):
     searchtextarea="#workbench\.view\.search > div > div > div.monaco-scrollable-element > div.split-view-container > div > div > div.pane-body > div.search-view.actions-right > div.search-widgets-container > div.search-widget > div.search-container.input-box > div > div.monaco-scrollable-element > div.monaco-inputbox.idle > div > textarea"
     output_message = new_page.locator(searchtextarea).input_value()
     assert "createFromPrebuild" in output_message
+
